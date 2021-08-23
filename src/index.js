47824152a5ab4a89982c2d446bec36ac7d9fade4
Paulo Gomes da Cruz Junior <paulushc@gmail.com>
2021-08-23 05:55:35 -0700

    Inicio do projeto e hello world

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/api/hello', (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Ouvindo em ${port}`);
+});
